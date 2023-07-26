c99e2daf574a293515b5f5ed7a2d82ce9226ffab
SteinGaming <danialdaryab@gmail.com>
2023-07-26 23:31:32 +0200

    fix: added missing files

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+
+rootProject.name = "FishingQol"
+
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.fabricmc.net")
+        maven(url = "https://jitpack.io/")
+        maven(url = "https://maven.minecraftforge.net/")
+        maven(url = "https://repo.spongepowered.org/maven/")
+        maven(url = "https://repo.sk1er.club/repository/maven-releases/")
+        maven(url = "https://maven.architectury.dev/")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
+            }
+        }
+    }
+}
