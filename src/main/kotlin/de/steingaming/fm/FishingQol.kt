b7f90d6ad4a915d2804f743e1bce7be0b6740351
SteinGaming <danialdaryab@gmail.com>
2023-07-27 00:18:56 +0200

    feat: add toggle

--- src/main/kotlin/de/steingaming/fm/FishingQol.kt
+++ src/main/kotlin/de/steingaming/fm/FishingQol.kt
@@ -5,13 +5,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.minecraft.client.Minecraft
+import net.minecraft.client.settings.KeyBinding
+import net.minecraft.util.ChatComponentText
 import net.minecraft.util.Vec3
 import net.minecraftforge.client.event.sound.SoundEvent
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.client.registry.ClientRegistry
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
+import org.lwjgl.input.Keyboard
 import kotlin.random.Random
 
 
@@ -22,24 +27,47 @@ class FishingQol {
         const val MODVERSION = "1.0.0"
     }
 
+    var enabled = true
     val scope = CoroutineScope(Dispatchers.Default)
+
+    val keybind = KeyBinding("Toggle FishingQOL", Keyboard.KEY_I, "FishingQol")
 
     @EventHandler
     fun preinit(event: FMLPreInitializationEvent) {
         MinecraftForge.EVENT_BUS.register(this)
+        ClientRegistry.registerKeyBinding(keybind)
     }
+
     @SubscribeEvent
     fun onSound(event: SoundEvent.SoundSourceEvent) {
-        if ((event.name == "random.splash" || event.name == "game.player.swim.splash") && Minecraft.getMinecraft()?.thePlayer?.fishEntity != null) {
+        if (enabled && (event.name == "random.splash" || event.name == "game.player.swim.splash") && Minecraft.getMinecraft()?.thePlayer?.fishEntity != null) {
             if (Minecraft.getMinecraft().thePlayer.fishEntity.positionVector.distanceTo(
                     Vec3(event.sound.xPosF.toDouble(), event.sound.yPosF.toDouble(), event.sound.zPosF.toDouble())
-                ) > 2.0) return
+                ) > 2.0
+            ) return
             scope.launch {
                 delay(Random.nextLong(200, 400))
-                Minecraft.getMinecraft().playerController.sendUseItem(Minecraft.getMinecraft().thePlayer, Minecraft.getMinecraft().theWorld, Minecraft.getMinecraft().thePlayer.heldItem)
+                Minecraft.getMinecraft().playerController.sendUseItem(
+                    Minecraft.getMinecraft().thePlayer,
+                    Minecraft.getMinecraft().theWorld,
+                    Minecraft.getMinecraft().thePlayer.heldItem
+                )
                 delay(Random.nextLong(300, 600))
-                Minecraft.getMinecraft().playerController.sendUseItem(Minecraft.getMinecraft().thePlayer, Minecraft.getMinecraft().theWorld, Minecraft.getMinecraft().thePlayer.heldItem)
+                Minecraft.getMinecraft().playerController.sendUseItem(
+                    Minecraft.getMinecraft().thePlayer,
+                    Minecraft.getMinecraft().theWorld,
+                    Minecraft.getMinecraft().thePlayer.heldItem
+                )
             }
         }
+    }
+
+    @SubscribeEvent
+    fun onTick(event: TickEvent.ClientTickEvent) {
+        if (keybind.isPressed) {
+            Minecraft.getMinecraft().thePlayer.addChatMessage(ChatComponentText(if (enabled) "§cFishingQol has been disabled!" else "§aFishingQol has been enabled!"))
+            enabled = !enabled
+        }
+
     }
 }
