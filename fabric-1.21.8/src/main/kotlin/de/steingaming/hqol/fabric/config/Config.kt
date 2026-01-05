39e0cb67e5195725d26d5861731799921252ceec
SteinGaming <danialdaryab@gmail.com>
2026-01-05 19:23:44 +0100

    fix(1.21.8): actually save the file
    moulconfig 1.21.x doesn't actually acknowledge the ESC press, as it is closed before handleKeyboardPress is called again. Do this ourselves.

--- fabric-1.21.8/src/main/kotlin/de/steingaming/hqol/fabric/config/Config.kt
+++ fabric-1.21.8/src/main/kotlin/de/steingaming/hqol/fabric/config/Config.kt
@@ -3,23 +3,40 @@ package de.steingaming.hqol.fabric.config
 import com.google.gson.annotations.Expose
 import de.steingaming.hqol.fabric.HypixelQolFabric
 import de.steingaming.hqol.fabric.config.categories.Fishing
+import de.steingaming.hqol.fabric.config.categories.Rift
 import io.github.notenoughupdates.moulconfig.annotations.Category
-import io.github.notenoughupdates.moulconfig.common.IMinecraft
 import io.github.notenoughupdates.moulconfig.gui.GuiContext
 import io.github.notenoughupdates.moulconfig.gui.GuiElementComponent
 import io.github.notenoughupdates.moulconfig.gui.MoulConfigEditor
 import io.github.notenoughupdates.moulconfig.platform.MoulConfigScreenComponent
 import io.github.notenoughupdates.moulconfig.processor.ConfigProcessorDriver
 import io.github.notenoughupdates.moulconfig.processor.MoulConfigProcessor
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.util.InputUtil
 import net.minecraft.text.Text
 import io.github.notenoughupdates.moulconfig.Config as MoulConfig
 
 
 class Config: MoulConfig() {
+    @Transient
+    var prevTickScreen: Screen? = null
     init {
         saveRunnables.add {
             HypixelQolFabric.INSTANCE.configManager.save()
+        }
+        ClientTickEvents.START_CLIENT_TICK.register {
+            val isPressed = InputUtil.isKeyPressed(MinecraftClient.getInstance().window.handle, 256)
+            if (isPressed && prevTickScreen is MoulConfigScreenComponent) {
+                val moulScreen = prevTickScreen as MoulConfigScreenComponent
+                val editor = (moulScreen.guiContext.root as GuiElementComponent).element as MoulConfigEditor<*>
+                if (editor.configObject is Config) {
+                    editor.configObject.saveNow()
+                }
+            }
+            prevTickScreen = MinecraftClient.getInstance().currentScreen
         }
     }
 
@@ -34,7 +51,7 @@ class Config: MoulConfig() {
         driver.processConfig(this@Config)
 
         val editor = MoulConfigEditor(processor)
-
+        editor.setWide(true)
         return MoulConfigScreenComponent(
             Text.empty(), GuiContext(GuiElementComponent(editor)), previousScreen
         )
