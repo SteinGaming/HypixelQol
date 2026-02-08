af657855f0d710485466d5f904e9c0360ba93bed
SteinGaming <danialdaryab@gmail.com>
2026-02-08 13:54:09 +0100

    fix(FastLeap): use attack keypress instead of attack event

--- fabric-1.21.10/src/main/java/de/steingaming/hqol/fabric/mixins/InjectMinecraftClient.java
+++ fabric-1.21.10/src/main/java/de/steingaming/hqol/fabric/mixins/InjectMinecraftClient.java
@@ -5,12 +5,15 @@ import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class InjectMinecraftClient {
-    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
-    public void onAttack(CallbackInfoReturnable<Boolean> cir) {
-        if (FastleapListener.punchEvent()) cir.cancel();
+    @Inject(method = "handleInputEvents", at = @At("HEAD"))
+    public void handleInputEvents(CallbackInfo ci) {
+        if (MinecraftClient.getInstance().options.attackKey.isPressed() && FastleapListener.punchEvent()) {
+            // Consume the press when FastleapListener uses the press
+            while (MinecraftClient.getInstance().options.attackKey.wasPressed());
+        }
     }
 }
