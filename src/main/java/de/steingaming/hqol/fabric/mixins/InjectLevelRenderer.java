83e18fe109c1c68e42f7d3de2247b0b5fcf27f83
SteinGaming <danialdaryab@gmail.com>
2026-04-29 21:42:55 +0200

    fix: 1.21.11 mixin not being correct

--- src/main/java/de/steingaming/hqol/fabric/mixins/InjectLevelRenderer.java
+++ src/main/java/de/steingaming/hqol/fabric/mixins/InjectLevelRenderer.java
@@ -3,9 +3,11 @@ package de.steingaming.hqol.fabric.mixins;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
 import de.steingaming.hqol.fabric.helper.RenderHelper;
+import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.chunk.ChunkSectionsToRender;
+import org.joml.Matrix4f;
 import org.joml.Matrix4fc;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,8 +26,8 @@ public class InjectLevelRenderer {
     //?} else {
     
     /*@Inject(method = "renderLevel", at = @At(value = "HEAD"))
-    private void beginRender(GraphicsResourceAllocator allocator, DeltaTracker tickCounter, boolean renderBlockOutline, net.minecraft.client.Camera camera, org.joml.Matrix4f positionMatrix, org.joml.Matrix4f matrix4f, org.joml.Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, org.joml.Matrix4f fogColor, boolean renderSky, CallbackInfo ci) {
-        RenderHelper.beginRender(tickCounter);
+    private void beginRender(GraphicsResourceAllocator graphicsResourceAllocator, DeltaTracker deltaTracker, boolean bl, Camera camera, Matrix4f matrix4f, Matrix4f matrix4f2, Matrix4f matrix4f3, GpuBufferSlice gpuBufferSlice, Vector4f vector4f, boolean bl2, CallbackInfo ci) {
+        RenderHelper.beginRender(deltaTracker);
     }
      
     *///?}
